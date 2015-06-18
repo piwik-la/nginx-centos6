da092be2ec22ca448a9ac2b3bf4259d076a14eae
piwik-la <admin@piwik.la>
2015-06-19 00:35:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ sudo yum install epel-release
 sudo yum install mysql-server
 sudo /etc/init.d/mysqld restart
 sudo /usr/bin/mysql_secure_installation
-sudo yum install nginx
+su -c 'rpm -Uhv http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm'
+su -c 'yum install -y nginx'
 sudo /etc/init.d/nginx start
 sudo yum install php-fpm php-mysql
 sudo chkconfig --levels 235 mysqld on
